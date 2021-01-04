c118f47a499224699c813fc2e4f74799052c997b
weshy007 <josephwaweru96@gmail.com>
2021-01-05 00:06:03 +0300

    the code failed at some point

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,74 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.toggle('flip');
+
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+// function checkForMatch() {
+//     if (firstCard.dataset.framework === secondCard.dataset.framework) {
+//         disableCards();
+//         return;
+//     }
+//     unflipCards();
+// }
+
+let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+isMatch ? disableCards() : unflipCards();
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        //lockBoard = false;
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+if (this === firstCard) return;
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+  function shuffle() {
+    cards.forEach(card => {
+      let ramdomPos = Math.floor(Math.random() * 12);
+      card.style.order = ramdomPos;
+    });
+  }
